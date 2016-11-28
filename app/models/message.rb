1a79e64f98ca11ba716582fca3588c9dec3fe32a
saratkavuru <kavurusarat@gmail.com>
2016-11-28 04:36:15 -0500

    model validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,6 @@ class Message < ActiveRecord::Base
   belongs_to :chat
   sync :all
   sync_scope :by_chat, ->(chat) { where(chat_id: chat.id) }
-
+  validates_presence_of :body, :chat_id, :user_id
 
 end
