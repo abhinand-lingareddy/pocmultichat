bcb8c8d3d9cb96d485da526d8bd8498f83226a1b
saratkavuru <kavurusarat@gmail.com>
2016-12-13 19:41:55 -0500

    clear

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115040744) do
+ActiveRecord::Schema.define(version: 20161202030440) do
 
   create_table "chats", force: :cascade do |t|
     t.string   "name"
